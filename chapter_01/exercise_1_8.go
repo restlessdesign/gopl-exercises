0d08fde95fa9b917c05bd1b5b084da54d91d5264
Kevin Sweeney <restlessdesign@users.noreply.github.com>
2016-09-05 09:39:58 -0400

    Add HTTP protocol if missing

---
+++ chapter_01/exercise_1_8.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if strings.HasPrefix(url, "http://") != true {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+			fmt.Fprintf(os.Stderr, "Fetch: Reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		// Be sure to close body
+		resp.Body.Close()
+	}
+}
