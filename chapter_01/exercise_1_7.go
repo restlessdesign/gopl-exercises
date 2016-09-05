6cef21c5a86aae3025bf68ba9f9139edd14aa08b
Kevin Sweeney <restlessdesign@users.noreply.github.com>
2016-09-05 09:17:28 -0400

    Improve GET tutorial performance
    Copy from resp.Body directly to stdout instead of first allocating a
    buffer and then printing out

---
+++ chapter_01/exercise_1_7.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+			fmt.Fprintf(os.Stderr, "Fetch: Reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		// Be sure to close body
+		resp.Body.Close()
+	}
+}
