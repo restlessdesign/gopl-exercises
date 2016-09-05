8f9d5f0333b412e4c11ef884935feb9b6e71936d
Kevin Sweeney <restlessdesign@users.noreply.github.com>
2016-09-05 09:16:15 -0400

    Close response body

--- chapter_01/05_fetch.go
+++ chapter_01/05_fetch.go
@@ -17,6 +17,10 @@ func main() {
 		}
 
 		bod, err := ioutil.ReadAll(resp.Body)
+
+		// Be sure to close body
+		resp.Body.Close()
+
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Fetch: Reading %s: %v\n", url, err)
 			os.Exit(1)
